cd57781dd90ff6a10e666456a7d2fb68c787523d
snail5039-code <snail5039@gmail.com>
2025-12-05 08:57:14 +0900

    잘 합쳐졋나

--- src/main/java/com/example/demo/dto/WorkLog.java
+++ src/main/java/com/example/demo/dto/WorkLog.java
@@ -22,7 +22,7 @@ public class WorkLog {
 	
 	private int memberId; //여기 두개는 나중에 그냥 다른 테이블에서 받아와도 되지 않을까 함
 	private int boardId; 
-	
+    private String templateType; 
 	private List<FileAttach> fileAttaches; //받아온 값 보여주려면 이거 있어야됌
 	
 }
